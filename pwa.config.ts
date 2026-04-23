5987d81a6086bd1729d7343072b8f36844dc6035
Rangsey P. HENG <rangseyheng@puc.edu.kh>
2026-04-23 21:09:07 +0700

    Update pwa.config.ts

--- pwa.config.ts
+++ pwa.config.ts
@@ -2,46 +2,17 @@ import { defineConfig } from '@/types/config';
 
 export default defineConfig({
   manifest: {
-    id: '/',
-    name: 'My Blog',
-    short_name: 'My Blog',
-    description: 'My blog description goes here...',
-    dir: 'auto',
-    lang: 'en-US',
-    background_color: '#fff',
-    theme_color: '#fff',
-    display: 'standalone',
-    orientation: 'natural',
-    scope: '/',
-    start_url: '/?utm_source=homescreen',
-    prefer_related_applications: false,
-    shortcuts: [
-      {
-        name: 'Shortcut 1',
-        short_name: 'Shortcut 1',
-        description: 'Shortcut 1 description goes here...',
-        url: '/search/label/shortcut-1?utm_source=homescreen',
-      },
-      {
-        name: 'Shortcut 2',
-        short_name: 'Shortcut 2',
-        description: 'Shortcut 2 description goes here...',
-        url: '/search/label/shortcut-2?utm_source=homescreen',
-      },
-    ],
+    name: "ផ្ទះរង្សី — RANGSEYhome",
+    short_name: "RANGSEYhome",
+    description: "Personal blog & portfolio by Rangsey P. HENG",
+    theme_color: "#55c489",
+    background_color: "#efefef",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
   },
-
+  origin: "https://rangseyhome.insightune.com", // Your custom domain
   pwa: {
     logs: true,
   },
-
-  // OneSignal is only available if you are using cloudflare workers
-  oneSignal: {
-    enabled: false, // To enable OneSignal, set this to true
-    appId: '********-****-****-****-************', // Replace with your OneSignal App Id
-    allowLocalhostAsSecureOrigin: true,
-  },
-
-  // Please replace with your blog url if you are using CDN (JsDelivr)
-  origin: 'https://rangseyhome.insightune.com',
 });
